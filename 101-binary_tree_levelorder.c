cb871b3c4900b9404b6e36ab50b53bacbbae2408
noblet <appiahpatricknoblet@gmail.com>
2024-03-27 22:48:15 +0000

    ADDED:  a function that goes through a binary tree using level-order traversal

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,50 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_levelorder - Performs level-order traversal of a binary tree
+ * @tree: Pointer to the root node of the tree to traverse
+ * @func: Pointer to a function to call for each node
+ *        The value in the node must be passed as a parameter to this function
+ * If tree or func is NULL, do nothing
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	binary_tree_t *current;
+
+	if (tree == NULL || func == NULL)
+		return;
+
+	/* Create a queue for level-order traversal */
+	binary_tree_t *queue[1024];
+	int front = 0, rear = -1;
+	int size = 0;
+
+	/* Enqueue the root node */
+	queue[++rear] = (binary_tree_t *)tree;
+	size++;
+
+	/* Perform level-order traversal */
+	while (size > 0)
+	{
+		/* Dequeue a node from the queue */
+		current = queue[front++];
+		size--;
+
+		/* Call the function with the value of the current node */
+		func(current->n);
+
+		/* Enqueue the left child, if exists */
+		if (current->left != NULL)
+		{
+			queue[++rear] = current->left;
+			size++;
+		}
+
+		/* Enqueue the right child, if exists */
+		if (current->right != NULL)
+		{
+			queue[++rear] = current->right;
+			size++;
+		}
+	}
+}
--- binary_trees.h
+++ binary_trees.h
@@ -41,10 +41,11 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second);
-
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_full(const binary_tree_t *tree);
 
 /*Helper functions*/
 /* int is_ancestor(const binary_tree_t *ancestor, const binary_tree_t *node)*/
-int binary_tree_is_full(const binary_tree_t *tree);
+
 
 #endif /* _BINARY_TREES_H_ */
