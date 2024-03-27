20f7f8dcc6af370cce49f8a03070bcaab907e381
noblet <appiahpatricknoblet@gmail.com>
2024-03-27 20:02:31 +0000

    added a function that finds the lowest common ancestor of two nodes

--- binary_trees.h
+++ binary_trees.h
@@ -21,8 +21,8 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
-/* Function prototypes */
 
+/* Function prototypes */
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -39,4 +39,10 @@ size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+
+
+/*Helper functions*/
+int is_ancestor(const binary_tree_t *ancestor, const binary_tree_t *node);
+
 #endif /* _BINARY_TREES_H_ */
